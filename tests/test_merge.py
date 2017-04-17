beb66ee631dfb4211fccf32dd2d6ef8aca0419f9
Dave Vandenbout <devb@xess.com>
2017-04-17 09:52:58 -0400

    Additional test cases were created.

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -5,7 +5,7 @@ from .setup_teardown import *
 def test_net_merge_1():
     a = Net('A')
     b = Net('B')
-    a += Pin(), Pin(), Pin(), Pin(), Pin()
+    a += 5 * Pin()
     assert len(a) == 5
     b += Pin(), Pin(), Pin()
     assert len(b) == 3
