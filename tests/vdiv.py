cde860bd3ddbc7134bd9a46a39ed8b686f58375e
Dave Vandenbout <devb@xess.com>
2019-01-25 16:43:26 -0500

    Added simple voltage divider example to test directory.

---
+++ tests/vdiv.py
@@ -0,0 +1,16 @@
+from skidl import *
+
+# Create input & output voltages and ground reference.
+vin, vout, gnd = Net('VI'), Net('VO'), Net('GND')
+
+# Create two resistors.
+r1, r2 = 2 * Part('device', 'R', TEMPLATE, footprint='Resistors_SMD:R_0805')
+r1.value = '1K'   # Set upper resistor value.
+r2.value = '500'  # Set lower resistor value.
+
+# Connect the nets and resistors.
+vin += r1[1]      # Connect the input to the upper resistor.
+gnd += r2[2]      # Connect the lower resistor to ground.
+vout += r1[2], r2[1] # Output comes from the connection of the two resistors.
+
+print(generate_netlist())
