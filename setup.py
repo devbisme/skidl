e64a8a72c0c6ad3c523bb1723b6b3dd177bc3264
johnthagen <johnthagen@users.noreply.github.com>
2019-12-08 20:51:02 -0500

    Document Python 3.8 support

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     test_suite="tests",
     tests_require=test_requirements,
