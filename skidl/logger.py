907b626743e0a0a48bc47580f269618efdb4b399
xesscorp <xesscorp@xess.com>
2020-02-09 10:39:06 -0500

    Split logging functions from utilities.py into its own file.

---
+++ skidl/logger.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+# MIT license
+#
+# Copyright (C) 2019 by XESS Corp.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""
+Logging for generic messages and ERC.
+"""
+
+import logging
+import sys
+from .utilities import get_script_name
+
+
+class CountCalls(object):
+    """Decorator for counting the number of times a function is called.
+
+    This is used for counting errors and warnings passed to logging functions,
+    making it easy to track if and how many errors/warnings were issued.
+    """
+
+    def __init__(self, func):
+        self.func = func
+        self.count = 0
+
+    def __call__(self, *args, **kwargs):
+        self.count += 1
+        return self.func(*args, **kwargs)
+
+    def reset(self):
+        self.count = 0
+
+
+def _create_logger(title, log_msg_id="", log_file_suffix=".log"):
+    """
+    Create a logger, usually for run-time errors or ERC violations.
+    """
+
+    logger = logging.getLogger(title)
+
+    # Errors & warnings always appear on the terminal.
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setLevel(logging.WARNING)
+    handler.setFormatter(logging.Formatter(log_msg_id + "%(levelname)s: %(message)s"))
+    logger.addHandler(handler)
+
+    # Errors and warnings are stored in a log file with the top-level script's name.
+    handler = logging.StreamHandler(open(get_script_name() + log_file_suffix, "w"))
+    handler.setLevel(logging.WARNING)
+    handler.setFormatter(logging.Formatter(log_msg_id + "%(levelname)s: %(message)s"))
+    logger.addHandler(handler)
+
+    # Set logger to trigger on info, warning, and error messages.
+    logger.setLevel(logging.INFO)
+
+    # Augment the logger's functions to count the number of errors and warnings.
+    logger.error = CountCalls(logger.error)
+    logger.warning = CountCalls(logger.warning)
+
+    return logger
+
+
+###############################################################################
+# Set up loggers for runtime messages and ERC reports.
+
+logger = _create_logger("skidl")
+erc_logger = _create_logger("ERC_Logger", "ERC ", ".erc")
+
+###############################################################################
