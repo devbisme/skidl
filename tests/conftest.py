2dc9c70351a62c1c8bee9aa5ffebb11a325c4ead
xesscorp <xesscorp@xess.com>
2020-02-10 11:30:58 -0500

    Re-enabled skip of SPICE tests for Python 2.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,6 @@ sys.path.append(os.path.join(os.getcwd(), "."))
 sys.path.append(os.path.join(os.getcwd(), ".."))
 
 # PySpice doesn't run under Python 2, so ignore that test file.
-# collect_ignore = []
-# if sys.version_info[0] <= 2:
-#     collect_ignore.append("test_spice.py")
+collect_ignore = []
+if sys.version_info[0] <= 2:
+    collect_ignore.append("test_spice.py")
