4f789095164721302e3d3f25ad10607cb49f2320
shanemmattner <shanemmattner@gmail.com>
2021-08-27 11:17:33 -0700

    started cubemx project.  Added decoupling and bulking caps to mcu

--- examples/stm32_usb_buck/subcircuits.py
+++ examples/stm32_usb_buck/subcircuits.py
@@ -19,29 +19,47 @@ def stm32f405r(vdd, gnd, v_5v):
     # MCU
     u = Part("MCU_ST_STM32F4", 'STM32F405RGTx', footprint='LQFP-64_10x10mm_P0.5mm')
 
-    l_vdd += u.p1, u.p19, u.p32, u.p48, u.p64, u.p13
+    
+
+    u.p5.label = 'OSC_IN'
+    u.p6.label = 'OSC_OUT'
+    u.p7.label = 'NRST'
+    u.p8.label = 'HB' #heartbeat
+    u.p16.label = 'USART2_TX'
+    u.p17.label = 'USART2_TX'
     u.p31.label = 'vcap1'
     u.p47.label = 'vcap2'
-    u.p8.label = 'HB' #heartbeat
-    u.p43.label = 'USB_P'
+    u.p45.label = 'USB_P'
     u.p44.label = 'USB_M'
+    u.p46.label = 'SWDIO'
+    u.p49.label = 'SWCLK'
+    u.p58.label = 'I2C1_SCL'
+    u.p59.label = 'I2C1_SDA'
     u.p60.label = 'BOOT0'
-    u.p7.label = 'NRST'
-    u.p35.label = 'BD_SEL'
 
-    vcap1 = Part("Device", 'C_Small', footprint='C_0603_1608Metric')
+    bcap = Part("Device", 'C_Small', footprint='C_0603_1608Metric', value='4.7uF')
+    fcap1 = Part("Device", 'C_Small', footprint='C_0603_1608Metric', value='100nF')
+    fcap2 = Part("Device", 'C_Small', footprint='C_0603_1608Metric', value='100nF')
+    fcap3 = Part("Device", 'C_Small', footprint='C_0603_1608Metric', value='100nF')
+    fcap4 = Part("Device", 'C_Small', footprint='C_0603_1608Metric', value='100nF')
+    fcap5 = Part("Device", 'C_Small', footprint='C_0603_1608Metric', value='100nF')
+
+    vcap1 = Part("Device", 'C_Small', footprint='C_0603_1608Metric', value='2.2uF')
     vcap1.p2 += u.p31
 
-    vcap2 = Part("Device", 'C_Small', footprint='C_0603_1608Metric')
+    vcap2 = Part("Device", 'C_Small', footprint='C_0603_1608Metric', value='2.2uF')
     vcap2.p2 += u.p47
     usb(l_5v, l_gnd, u.p43, u.p44, True)
     boot_sw(l_vdd, l_gnd)
-    bd_en(l_vdd, l_gnd, u.p35)
     led(u.p8, l_gnd, 'blue', '5.6k')
-    
-    
-    l_gnd += vcap1.p1, vcap2.p1, u.p12, u.p18, u.p63
 
+    l_vdd += u.p1, u.p19, u.p32, u.p48, u.p64, u.p13, bcap.p1, fcap1.p1, fcap2.p1, fcap3.p1, fcap4.p1, fcap5.p1
+    l_gnd += vcap1.p1, vcap2.p1, u.p12, u.p18, u.p63, bcap.p2, fcap1.p2, fcap2.p2, fcap3.p2, fcap4.p2, fcap5.p2
+
+
+@SubCircuit
+def oscillator(vdd, gnd, osc_in, osc_out):
+    print("oscillator")
 
 @SubCircuit
 def boot_sw(vdd, gnd):
