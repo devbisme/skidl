5017079e73d4665ee7b77c5cfaf2a95328de22db
devbisme <devb@xess.com>
2022-12-06 11:23:59 -0500

    test: Added test of schematic generation with connections between hierarchical levels (which fails).

--- tests/test_schematic_gen.py
+++ tests/test_schematic_gen.py
@@ -145,3 +145,22 @@ def test_schematic_gen_hier():
             vcc & q2["E"]
 
     generate_schematic(filepath=create_output_dir("hier"), flatness=0.0)
+
+def test_schematic_hier_connections():
+
+    r = Part("Device.lib", "R", footprint="Resistor_SMD:R_0805_2012Metric",dest=TEMPLATE)
+    q = Part(lib="Device.lib", name="Q_PNP_CBE", footprint="Package_TO_SOT_SMD:SOT-223-3_TabPin2", dest=TEMPLATE, symtx="V")
+
+    a = Net("A")
+    b = Net("B")
+    o = Net("O")
+
+    for _ in range(3):
+        with Group("A:"):
+            q1 = q()
+            q2 = q()
+            r1, r2, r3 = r(3, value="10K")
+            a & r1 & (q1['c,e'] | q2['c,e']) & r3 & o
+            b & r2 & (q1['b'] | q2['b'])
+
+    generate_schematic(filepath=create_output_dir("hier_connections"), flatness=1.0)
