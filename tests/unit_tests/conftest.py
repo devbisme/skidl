8a151095dbb51d471857263c14b80d43185ac7f4
devbisme <devb@xess.com>
2022-12-22 13:47:38 -0500

    docs: Added missing period.

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.join(os.getcwd(), "."))
 sys.path.append(os.path.join(os.getcwd(), ".."))
 
 # PySpice doesn't run under Python 2, so ignore that test file.
-# Keyword-only arguments were introduced in py3
+# Keyword-only arguments were introduced in py3.
 collect_ignore = []
 if sys.version_info.major <= 2:
     collect_ignore.append("test_spice.py")
