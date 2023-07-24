2469693746363d15cf5926f5ce97d01ccfab2030
devbisme <devb@xess.com>
2023-07-24 12:02:16 -0400

    refactor: Removed netlist generation for KiCad 5 from main file.

--- src/skidl/tools/kicad/kicad.py
+++ src/skidl/tools/kicad/kicad.py
@@ -644,77 +644,77 @@ def gen_netlist(circuit):
     return netlist
 
 
-@export_to_all
-def gen_netlist_comp(part):
-    """Generate the netlist text describing a component.
+# @export_to_all
+# def gen_netlist_comp(part):
+#     """Generate the netlist text describing a component.
 
-    Args:
-        part (Part): Part object.
+#     Args:
+#         part (Part): Part object.
 
-    Returns:
-        str: String containing component netlist description.
-    """
+#     Returns:
+#         str: String containing component netlist description.
+#     """
 
-    from skidl.circuit import HIER_SEP
+#     from skidl.circuit import HIER_SEP
 
-    ref = add_quotes(part.ref)
+#     ref = add_quotes(part.ref)
 
-    value = add_quotes(part.value_str)
+#     value = add_quotes(part.value_str)
 
-    footprint = getattr(part, "footprint", "")
-    footprint = add_quotes(footprint)
+#     footprint = getattr(part, "footprint", "")
+#     footprint = add_quotes(footprint)
 
-    lib_filename = getattr(getattr(part, "lib", ""), "filename", "NO_LIB")
-    part_name = add_quotes(part.name)
+#     lib_filename = getattr(getattr(part, "lib", ""), "filename", "NO_LIB")
+#     part_name = add_quotes(part.name)
 
-    # Embed the hierarchy along with a random integer into the sheetpath for each component.
-    # This enables hierarchical selection in pcbnew.
-    hierarchy = add_quotes("/" + part.hierarchical_name.replace(HIER_SEP, "/"))
-    tstamps = hierarchy
+#     # Embed the hierarchy along with a random integer into the sheetpath for each component.
+#     # This enables hierarchical selection in pcbnew.
+#     hierarchy = add_quotes("/" + part.hierarchical_name.replace(HIER_SEP, "/"))
+#     tstamps = hierarchy
 
-    fields = ""
-    for fld_name, fld_value in part.fields.items():
-        fld_value = add_quotes(fld_value)
-        if fld_value:
-            fld_name = add_quotes(fld_name)
-            fields += "\n        (field (name {fld_name}) {fld_value})".format(
-                **locals()
-            )
-    if fields:
-        fields = "      (fields" + fields
-        fields += ")\n"
+#     fields = ""
+#     for fld_name, fld_value in part.fields.items():
+#         fld_value = add_quotes(fld_value)
+#         if fld_value:
+#             fld_name = add_quotes(fld_name)
+#             fields += "\n        (field (name {fld_name}) {fld_value})".format(
+#                 **locals()
+#             )
+#     if fields:
+#         fields = "      (fields" + fields
+#         fields += ")\n"
 
-    template = (
-        "    (comp (ref {ref})\n"
-        + "      (value {value})\n"
-        + "      (footprint {footprint})\n"
-        + "{fields}"
-        + "      (libsource (lib {lib_filename}) (part {part_name}))\n"
-        + "      (sheetpath (names {hierarchy}) (tstamps {tstamps})))"
-    )
-    txt = template.format(**locals())
-    return txt
+#     template = (
+#         "    (comp (ref {ref})\n"
+#         + "      (value {value})\n"
+#         + "      (footprint {footprint})\n"
+#         + "{fields}"
+#         + "      (libsource (lib {lib_filename}) (part {part_name}))\n"
+#         + "      (sheetpath (names {hierarchy}) (tstamps {tstamps})))"
+#     )
+#     txt = template.format(**locals())
+#     return txt
 
 
-@export_to_all
-def gen_netlist_net(net):
-    """Generate the netlist text describing a net.
+# @export_to_all
+# def gen_netlist_net(net):
+#     """Generate the netlist text describing a net.
 
-    Args:
-        part (Net): Net object.
+#     Args:
+#         part (Net): Net object.
 
-    Returns:
-        str: String containing net netlist description.
-    """
-    code = add_quotes(net.code)
-    name = add_quotes(net.name)
-    txt = "    (net (code {code}) (name {name})".format(**locals())
-    for p in sorted(net.pins, key=str):
-        part_ref = add_quotes(p.part.ref)
-        pin_num = add_quotes(p.num)
-        txt += "\n      (node (ref {part_ref}) (pin {pin_num}))".format(**locals())
-    txt += ")"
-    return txt
+#     Returns:
+#         str: String containing net netlist description.
+#     """
+#     code = add_quotes(net.code)
+#     name = add_quotes(net.name)
+#     txt = "    (net (code {code}) (name {name})".format(**locals())
+#     for p in sorted(net.pins, key=str):
+#         part_ref = add_quotes(p.part.ref)
+#         pin_num = add_quotes(p.num)
+#         txt += "\n      (node (ref {part_ref}) (pin {pin_num}))".format(**locals())
+#     txt += ")"
+#     return txt
 
 
 @export_to_all
