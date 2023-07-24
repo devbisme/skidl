6d3ebd3b4632e339ca8e1e6a90669ffeb65fbebe
devbisme <devb@xess.com>
2023-07-24 13:31:44 -0400

    refactor: Removed schematic generation for KiCad 5 from main file.

--- src/skidl/tools/kicad/__init__.py
+++ src/skidl/tools/kicad/__init__.py
@@ -3,14 +3,12 @@
 # The MIT License (MIT) - Copyright (c) 2016-2021 Dave Vandenbout.
 
 from . import constants
-from .to_eeschema import *
+from .bboxes import calc_symbol_bbox, calc_hier_label_bbox
+from .gen_schematic import *
 from .gen_netlist import gen_netlist
 from .gen_xml import gen_xml
 from .kicad import (
-    calc_symbol_bbox,
-    calc_hier_label_bbox,
     gen_pcb,
-    gen_schematic,
     gen_svg_comp,
     get_kicad_lib_tbl_dir,
     lib_suffix,
