ff0282e38d870c2aeb11319158a351f2bccd170a
devbisme <devb@xess.com>
2023-07-26 13:13:28 -0400

    Release 1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 import setuptools
 
-__version__ = "1.3.0-alpha"
+__version__ = "1.2.1"
 __author__ = "Dave Vandenbout"
 __email__ = "devb@xess.com"
 
--- src/skidl/pckg_info.py
+++ src/skidl/pckg_info.py
@@ -1,3 +1,3 @@
-__version__ = "1.3.0-alpha"
+__version__ = "1.2.1"
 __author__ = "Dave Vandenbout"
 __email__ = "devb@xess.com"
