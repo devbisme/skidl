250b8a6c138419285526c4c49544c01c477c604c
devbisme <devb@xess.com>
2024-07-11 09:27:18 -0400

    fix: Fixed library search paths for SVG generation examples.

--- tests/examples/svg/svg_setup.py
+++ tests/examples/svg/svg_setup.py
@@ -1,7 +1,7 @@
 from skidl import set_default_tool, lib_search_paths, KICAD5, KICAD6
 import os
 
-tool = KICAD6
+tool = KICAD5
 set_default_tool(tool)
 lib_dir = os.path.join("..", "..", "test_data", tool)
-lib_search_paths[tool].append(lib_dir)
+lib_search_paths[tool] = [lib_dir,]
