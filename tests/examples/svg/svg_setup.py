f28d6b8f9be6b13e388d1dbeb79ce5f74c5427ab
devbisme <devb@xess.com>
2024-07-12 16:17:46 -0400

    test: Changed version for generating SVG schematics to KiCad 8.

--- tests/examples/svg/svg_setup.py
+++ tests/examples/svg/svg_setup.py
@@ -1,7 +1,7 @@
-from skidl import set_default_tool, lib_search_paths, KICAD5, KICAD6
+from skidl import set_default_tool, lib_search_paths, KICAD5, KICAD6, KICAD7, KICAD8
 import os
 
-tool = KICAD6
+tool = KICAD8
 set_default_tool(tool)
 lib_dir = os.path.join("..", "..", "test_data", tool)
 lib_search_paths[tool].insert(0, lib_dir)
