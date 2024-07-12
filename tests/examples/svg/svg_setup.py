893cc167179efba52c2e93f2c44a44d28417e2df
devbisme <devb@xess.com>
2024-07-12 14:52:43 -0400

    fix: Add test libraries to the beginning of the lib_search_paths but retain the search of specialized libs that exist on my local machine.

--- tests/examples/svg/svg_setup.py
+++ tests/examples/svg/svg_setup.py
@@ -4,4 +4,4 @@ import os
 tool = KICAD6
 set_default_tool(tool)
 lib_dir = os.path.join("..", "..", "test_data", tool)
-lib_search_paths[tool] = [lib_dir,]
+lib_search_paths[tool].insert(0, lib_dir)
