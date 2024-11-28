e1bb8bf9e27beed030855e23f32790695b6aee2e
devbisme <devb@xess.com>
2024-11-27 20:46:09 -0500

    docs: Updated documentation for version 2.0.0..

--- docs/api/html/_static/bizstyle.js
+++ docs/api/html/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("SKiDL 1.2.2 documentation");
+        $("li.nav-item-0 a").text("SKiDL 2.0.0 documentation");
     }
 });
