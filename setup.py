4ec4ffcc7a387293468735d459a436b644e627cc
devbisme <devb@xess.com>
2025-04-29 20:29:42 -0400

    chore: Removed unneeded install requirement for the future package.

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read().replace(".. :changelog:", "")
 
 requirements = [
-    "future >= 0.15.0",
     "sexpdata == 1.0.0",
     "kinparse >= 1.2.3",
     "kinet2pcb >= 1.1.2",
