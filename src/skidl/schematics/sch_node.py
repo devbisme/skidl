5c29721eecef4b88e43cdd3a8879b65d6e6e4a1b
devbisme <devb@xess.com>
2025-06-02 20:28:45 -0400

    SchNode is no longer a subclass of Node. They should be kept separate at this point.

--- src/skidl/schematics/sch_node.py
+++ src/skidl/schematics/sch_node.py
@@ -19,7 +19,8 @@ Node subclass used for generating schematics.
 
 
 @export_to_all
-class SchNode(Node, Placer, Router):
+# class SchNode(Node, Placer, Router):
+class SchNode(Placer, Router):
     """Data structure for holding information about a node in the circuit hierarchy."""
 
     filename_sz = 20
@@ -35,7 +36,10 @@ class SchNode(Node, Placer, Router):
         flatness=0.0,
     ):
         # Initialize using the Node superclass.
-        super().__init__(None, filepath, top_name)
+        # super().__init__(None)
+        self.parts = []
+        self.filepath = filepath
+        self.top_name = top_name
         self.parent = None
         self.children = defaultdict(
             lambda: type(self)(
@@ -56,6 +60,86 @@ class SchNode(Node, Placer, Router):
         if circuit:
             self.add_circuit(circuit)
 
+    def get_or_add_child(self, name):
+        """Get or create a child node with the given name and attach it to the parent node.
+
+        Args:
+            self (Node): The parent node.
+            name (str): Name of the child node.
+
+        Returns:
+            Node: The child node.
+        """
+
+        # This will create a new child node if it doesn't already exist.
+        child = self.children[name]
+
+        # Attach the child node to this node.
+        child.parent = self
+
+        return child 
+
+    def find_node_with_part(self, part):
+        """Find the node that contains the part based on its hierarchy.
+
+        Args:
+            part (Part): The part being searched for in the node hierarchy.
+
+        Returns:
+            Node: The Node object containing the part.
+        """
+
+        from skidl.circuit import HIER_SEP
+
+        level_names = part.hierarchical_name.split(HIER_SEP)
+        node = self
+        for lvl_nm in level_names[1:]:
+            node = node.children[lvl_nm]
+        assert part in node.parts
+        return node
+
+    def add_part(self, part, level=0):
+        """Add a part to the node at the appropriate level of the hierarchy.
+
+        Args:
+            part (Part): Part to be added to this node or one of its children.
+            level (int, optional): The current level (depth) of the node in the hierarchy. Defaults to 0.
+        """
+
+        from skidl.circuit import HIER_SEP
+
+        # Get list of names of hierarchical levels (in order) leading to this part.
+        level_names = part.hierarchical_name.split(HIER_SEP)
+
+        # Get depth in hierarchy for this part.
+        part_level = len(level_names) - 1
+        assert part_level >= level
+
+        # Node name is the name assigned to this level of the hierarchy.
+        self.name = level_names[level]
+
+        # File name for storing the schematic for this node.
+        base_filename = "_".join([self.top_name] + level_names[0 : level + 1]) + ".sch"
+        self.sheet_filename = base_filename
+
+        if part_level == level:
+            # Add part to node at this level in the hierarchy.
+            if not part.unit:
+                # Monolithic part so just add it to the node.
+                self.parts.append(part)
+            else:
+                # Multi-unit part so add each unit to the node.
+                # FIXME: Some part units might be split into other nodes.
+                for p in part.unit.values():
+                    self.parts.append(p)
+        else:
+            # Part is at a level below the current node. Get the child node using
+            # the name of the next level in the hierarchy for this part.
+            child_node = self.get_or_add_child(level_names[level + 1])
+
+            # Add part to the child node (or one of its children).
+            child_node.add_part(part, level + 1)
+
     def add_circuit(self, circuit):
         """Add parts in circuit to node and its children.
 
@@ -64,7 +148,10 @@ class SchNode(Node, Placer, Router):
         """
 
         # Build the circuit node hierarchy by adding the parts.
-        super().add_circuit(circuit)
+        # super().add_circuit(circuit)
+        # Build the circuit node hierarchy by adding the parts.
+        for part in circuit.parts:
+            self.add_part(part)
 
         # Add terminals to nodes in the hierarchy for nets that span across nodes.
         for net in circuit.nets:
@@ -123,6 +210,59 @@ class SchNode(Node, Placer, Router):
 
         nt = NetTerminal(net, self.tool_module)
         self.parts.append(nt)
+
+    def get_internal_nets(self):
+        """Return a list of nets that have at least one pin on a part in this node."""
+
+        processed_nets = []
+        internal_nets = []
+        for part in self.parts:
+            for part_pin in part:
+                # No explicit wire for pins connected to labeled stub nets.
+                if part_pin.stub:
+                    continue
+
+                # No explicit wires if the pin is not connected to anything.
+                if not part_pin.is_connected():
+                    continue
+
+                net = part_pin.net
+
+                # Skip nets that have already been processed.
+                if net in processed_nets:
+                    continue
+
+                processed_nets.append(net)
+
+                # Skip stubbed nets.
+                if getattr(net, "stub", False) is True:
+                    continue
+
+                # Add net to collection if at least one pin is on one of the parts of the node.
+                for net_pin in net.pins:
+                    if net_pin.part in self.parts:
+                        internal_nets.append(net)
+                        break
+
+        return internal_nets
+
+    def get_internal_pins(self, net):
+        """Return the pins on the net that are on parts in the node.
+
+        Args:
+            net (Net): The net whose pins are being examined.
+
+        Returns:
+            list: List of pins on the net that are on parts in this node.
+        """
+
+        # Skip pins on stubbed nets.
+        if getattr(net, "stub", False) is True:
+            return []
+
+        return [pin for pin in net.pins if pin.stub is False and pin.part in self.parts]
+
+
 
     def external_bbox(self):
         """Return the bounding box of a hierarchical sheet as seen by its parent node."""
