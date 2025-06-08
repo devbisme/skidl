2401a579c26baa3e3d44f21fb34537e528c31e6c
devbisme <devb@xess.com>
2025-06-08 19:25:12 -0400

    Removed old code that was commented out.

--- src/skidl/utilities.py
+++ src/skidl/utilities.py
@@ -945,7 +945,6 @@ class TriggerDict(dict):
         """
         if k in self.trigger_funcs:
             if v != self.get(k, None):
-            # if v != self[k]:
                 self.trigger_funcs[k](self, k, v)
         super().__setitem__(k, v)
 
