69906b2e664db0530f82a2186b9bae1012a5e729
devbisme <devb@xess.com>
2025-06-21 14:43:54 -0400

    SKiDL no longer runs under Python 3.7 because it uses Simp_sexp.

--- setup.py
+++ setup.py
@@ -75,7 +75,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
