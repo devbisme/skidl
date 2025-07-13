e376959d911016e2f095e50c500b54b20c1cb75e
devbisme <devb@xess.com>
2025-07-13 16:43:13 -0400

    Add netclass assignments to this example to check their inclusion in the netlist output.

--- tests/examples/traces/trace.py
+++ tests/examples/traces/trace.py
@@ -1,10 +1,16 @@
 from skidl import *
 
+netclass1 = NetClass("netclass1", priority=1)
+netclass2 = NetClass("netclass2", priority=2)
+netclass3 = NetClass("netclass3", priority=3)
+
 @subcircuit
 def rc():
     r = Part("Device","R")
     c = Part("Device","C")
     rc = r | c
+    r[1].net.netclass = netclass1
+    r[1].net.netclass = netclass2
     return Interface(p1=rc[1], p2=rc[0])
 
 @subcircuit
@@ -12,6 +18,7 @@ def rcrl():
     l = Part("Device","L")
     r = Part("Device","R")
     rl = l & r
+    l[2].net.netclass = netclass3
     rc_ = rc()
     rl[0] += rc_.p1
     rl[1] += rc_.p2
