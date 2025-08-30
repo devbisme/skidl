ea6f2ce29e9366bf03422aa40ad6dfc2e3f46caf
devbisme <devb@xess.com>
2025-08-30 16:05:13 -0400

    Update setup.py to read README.md instead of README.rst

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
     from distutils.core import setup
 
 
-with open("README.rst") as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 with open("HISTORY.rst") as history_file:
