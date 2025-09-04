15ee8d69e068be2adf385337b7ba88141daf8e66
devbisme <devb@xess.com>
2025-09-04 13:24:41 -0400

    Correct XSPICE examples that incorrectly used adc_bridge and dac_bridge.

--- tests/examples/spice/xspice_test.py
+++ tests/examples/spice/xspice_test.py
@@ -3,7 +3,7 @@ from skidl.pyspice import *
 lib_search_paths[SKIDL].append("../../../src/skidl/tools/skidl/libs")
 
 # Component declarations showing various XSPICE styles.
-vin = sinev(offset=1.65 @ u_V, amplitude=1.65 @ u_V, frequency=100e6)
+vin = sinev(offset=1.65 @ u_V, amplitude=1.65 @ u_V, frequency=1e4)
 
 adc = Part(
     "pyspice",
@@ -12,8 +12,8 @@ adc = Part(
     model=XspiceModel(
         "adc",
         "adc_bridge",
-        in_low=0.05 @ u_V,
-        in_high=0.1 @ u_V,
+        in_low=1.65 @ u_V,
+        in_high=1.65 @ u_V,
         rise_delay=1e-9 @ u_s,
         fall_delay=1e-9 @ u_s,
     ),
@@ -37,7 +37,7 @@ buf = buf_tmp()
 
 dac = A(
     io=["dig_in[]", "anlg_out[]"],
-    model=XspiceModel("dac", "dac_bridge", out_low=1.0 @ u_V, out_high=3.3 @ u_V),
+    model=XspiceModel("dac", "dac_bridge", out_low=0 @ u_V, out_high=3.3 @ u_V),
 )
 
 r = R(value=1 @ u_kOhm)
@@ -56,10 +56,10 @@ buf["buf_out"] += dac["dig_in"][0]
 # Attach first pin of DAC anlg_out vector to load resistor.
 r["p,n"] += dac["anlg_out"][0], gnd
 
-circ = generate_netlist(libs="SpiceLib")
+circ = generate_netlist(file_="xspice.cir", libs="SpiceLib")
 print(circ)
 sim = Simulator.factory().simulation(circ)
-waveforms = sim.transient(step_time=0.1 @ u_ns, end_time=50 @ u_ns)
+waveforms = sim.transient(step_time=1 @ u_us, end_time=2 @ u_ms)
 time = waveforms.time
 vin = waveforms[node(vin["p"])]
 vout = waveforms[node(r["p"])]
