9261fc3e8d7556e986f7c1da0712c8ceff38ec70
devbisme <devb@xess.com>
2025-09-10 19:55:48 -0400

    - SKiDL configuration now uses standard locations for storing .skidlcfg. - Configuration process now creates a storage directory for pickled part libraries and the part search database. - Use collections.abc.Mapping instead of deprecated collections.Mapping. - Use expand-path() utility function to create a fully-expanded path.

--- src/skidl/config_.py
+++ src/skidl/config_.py
@@ -14,14 +14,38 @@ settings that are merged according to priority.
 
 import collections
 import json
+import os
 import os.path
+import sys
 
 from .logger import active_logger
 from .part_query import footprint_cache
 from .scriptinfo import get_script_name
 from .tools import ALL_TOOLS, lib_suffixes, tool_modules
-from .utilities import TriggerDict, export_to_all, merge_dicts
+from .utilities import TriggerDict, export_to_all, merge_dicts, expand_path
 
+def _get_default_skidl_storage_dir():
+    """
+    Return path to a platform-appropriate per-user directory for persistent SKiDL data.
+
+    Linux/Unix: XDG_DATA_HOME/skidl or ~/.local/share/skidl
+    macOS: ~/Library/Application Support/skidl
+    Windows: %APPDATA%/skidl or %LOCALAPPDATA%/skidl (fallback to ~/AppData/Roaming/skidl)
+    """
+
+    # Determine the base directory for storing SKiDL data for a given OS.
+    if os.name == "nt":
+        appdata = os.getenv("APPDATA") or os.getenv("LOCALAPPDATA")
+        if appdata:
+            return os.path.join(appdata, "skidl")
+        return os.path.join(os.path.expanduser("~"), "AppData", "Roaming", "skidl")
+    else:
+        xdg = os.getenv("XDG_DATA_HOME")
+        if xdg:
+            return os.path.join(xdg, "skidl")
+        if sys.platform == "darwin":
+            return os.path.join(os.path.expanduser("~"), "Library", "Application Support", "skidl")
+        return os.path.join(os.path.expanduser("~"), ".local", "share", "skidl")
 
 @export_to_all
 class Config(dict):
@@ -92,7 +116,7 @@ class Config(dict):
             if (
                 k in self
                 and isinstance(self[k], dict)
-                and isinstance(merge_dct[k], collections.Mapping)
+                and isinstance(merge_dct[k], collections.abc.Mapping)
             ):
                 merge_dicts(self[k], merge_dct[k])
             else:
@@ -109,9 +133,7 @@ class Config(dict):
             *dirs: Directories to search for configuration files.
         """
         for dir in dirs:
-            path = os.path.join(dir, self.cfg_file_name)
-            path = os.path.expanduser(path)
-            path = os.path.abspath(path)
+            path = expand_path(os.path.join(dir, self.cfg_file_name))
             try:
                 with open(path) as cfg_fp:
                     self.merge(json.load(cfg_fp))
@@ -126,9 +148,7 @@ class Config(dict):
             dir (str, optional): Directory to store the configuration file in.
                 Defaults to the current directory.
         """
-        path = os.path.join(dir, self.cfg_file_name)
-        path = os.path.expanduser(path)
-        path = os.path.abspath(path)
+        path = expand_path(os.path.join(dir, self.cfg_file_name))
         with open(path, "w") as cfg_fp:
             json.dump(self, cfg_fp, indent=4)
 
@@ -148,8 +168,13 @@ class SkidlConfig(Config):
     def __init__(self, tool):
         from skidl import SKIDL
 
+        self.skidl_storage_dir = _get_default_skidl_storage_dir()
+
         # Load the .skidlcfg file from one of the list of directories.
-        super().__init__(".skidlcfg", "/etc", "~", ".")
+        super().__init__(".skidlcfg", "/etc", self.skidl_storage_dir, ".")
+
+        # Make the directory for storing persistent SKiDL data if it doesn't exist.
+        os.makedirs(self.skidl_storage_dir, exist_ok=True)
 
         # If no configuration files were found, set default backend/tool.
         if "tool" not in self:
@@ -157,7 +182,15 @@ class SkidlConfig(Config):
 
         # If no configuration files were found, set default directory for part library pickle files.
         if "pickle_dir" not in self:
-            self.pickle_dir = "./lib_pickle_dir"
+            self.pickle_dir = os.path.join(self.skidl_storage_dir, "lib_pickle_dir")
+        # Make the directory.
+        os.makedirs(self.pickle_dir, exist_ok=True)
+
+        # If no configuration files were found, set default directory for part search database.
+        if "part_search_db_dir" not in self:
+            self.part_search_db_dir = self.skidl_storage_dir
+        # Make the directory.
+        os.makedirs(self.part_search_db_dir, exist_ok=True)
 
         # If no configuration files were found, set some default part lib search paths.
         if "lib_search_paths" not in self:
@@ -187,3 +220,14 @@ class SkidlConfig(Config):
 
         self["footprint_search_paths"] = TriggerDict(self["footprint_search_paths"])
         self["footprint_search_paths"].trigger_funcs[self.tool] = invalidate_footprint_cache
+
+    def store(self, dir=None):
+        """
+        Store the current SKiDL configuration as a JSON file.
+        
+        Args:
+            dir (str, optional): Directory to store the configuration file in.
+                Defaults to the SKiDL storage directory.
+        """
+        dir = dir or self.skidl_storage_dir
+        super().store(dir)
