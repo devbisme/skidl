4a4b710fd226504ffa7a581197fd392a173278cb
devbisme <devb@xess.com>
2025-12-13 11:30:45 -0500

    Bumped release and updated docs.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 import setuptools
 
-__version__ = "2.2.0"
+__version__ = "2.2.1"
 __author__ = "Dave Vandenbout"
 __email__ = "dave@vdb.name"
 
--- src/skidl/pckg_info.py
+++ src/skidl/pckg_info.py
@@ -1,3 +1,3 @@
-__version__ = "2.2.0"
+__version__ = "2.2.1"
 __author__ = "Dave Vandenbout"
 __email__ = "dave@vdb.name"
